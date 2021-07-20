6d860dd487f4e9ad4cc4b76a4dfeb780a4f88d69
Andes Lam <andeslam223@gmail.com>
2021-07-20 11:00:08 +0800

    added FYPCategory

--- FYPCategory.sql
+++ FYPCategory.sql
@@ -1,3 +1,11 @@
 create table tblFypCategories(
-	
+	PK_tblFypCategories varchar(30) primary key,
+	constraint chk_FypCategories_PK CHECK (PK_tblFypCategories IN (
+	'Artificial Intelligence', 
+    'Computer Games', 'Computer Security', 'Database', 
+	'Embedded Systems and Software', 'Human Language Technology', 
+	'Miscellaneous', 'Mobile and Wireless Computing', 
+    'Mobile Applications', 'Mobile Gaming', 'Networking', 
+	'Operating Systems', 'Software Technology', 'Theory', 
+	'Vision and Graphics'))
 );
