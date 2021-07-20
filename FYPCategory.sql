e45b4cc3337533a637465bda1a9d90106745980e
Andes Lam <andeslam223@gmail.com>
2021-07-20 11:15:59 +0800

    inserted values into tblFypCategories

--- FYPCategory.sql
+++ FYPCategory.sql
@@ -9,3 +9,19 @@ create table tblFypCategories(
 	'Operating Systems', 'Software Technology', 'Theory', 
 	'Vision and Graphics'))
 );
+
+insert into tblFypCategories values ('Artificial Intelligence');
+insert into tblFypCategories values ('Computer Games');
+insert into tblFypCategories values ('Computer Security');
+insert into tblFypCategories values ('Database');
+insert into tblFypCategories values ('Embedded Systems and Software');
+insert into tblFypCategories values ('Human Language Technology');
+insert into tblFypCategories values ('Miscellaneous');
+insert into tblFypCategories values ('Mobile and Wireless Computing');
+insert into tblFypCategories values ('Mobile Applications');
+insert into tblFypCategories values ('Mobile Gaming');
+insert into tblFypCategories values ('Networking');
+insert into tblFypCategories values ('Operating Systems');
+insert into tblFypCategories values ('Software Technology');
+insert into tblFypCategories values ('Theory');
+insert into tblFypCategories values ('Vision and Graphics');
