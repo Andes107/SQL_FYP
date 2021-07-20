b9db30e98f2fe19a97b4f6a1e4240cda33d5fee4
Andes Lam <andeslam223@gmail.com>
2021-07-20 14:18:40 +0800

    modified Fyp

---
+++ FYP.sql
@@ -0,0 +1,25 @@
+create table tblFyp(
+	PK_tblFyp smallint primary key,
+	title varchar(100) not null,
+	description varchar(1200) not null,
+	category varchar(30) not null,
+	constraint FK_tblFyp_tblFypCategories foreign key(category)
+        references tblFypCategories(PK_tblFypCategories)
+		on delete cascade,
+	type varchar(7) not null,
+	constraint CHK_Fyp_type check (type in ('project','thesis')),
+	otherRequirements varchar(200),
+	minStudents smallint not null,
+	constraint DF_tblFyp_minStudents default 1,
+	/*
+	1. Default inline constraint
+		https://dba.stackexchange.com/a/159361
+	*/
+	maxStudents smallint not null,
+	constraint DF_tblFyp_maxStudents default 1,
+	isAvailable bit not null,
+	constraint DF_tblFyp_isAvailable default 1,
+	constraint CHK_Fyp_min_max_Students	check(
+		(minStudents between 1 and maxStudents) and
+		(maxStudents between minStudents and 4))
+);
