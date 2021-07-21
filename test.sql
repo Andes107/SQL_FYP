6d9eb7b836b9fa9a3ece15cd9518161d098f1281
Andes Lam <andeslam223@gmail.com>
2021-07-21 12:28:11 +0800

    added why shouldn't I abuse CRUD indexes due to the potential cost

--- test.sql
+++ test.sql
@@ -24,3 +24,21 @@ add constraint PK_tblTest primary key (PK_tblTest);
 	name. though the constraint appears not in the constraint
 	https://www.w3schools.com/sql/sql_primarykey.ASP
 */
+
+drop index PK_tblTest
+on tblTest;
+/*
+	1. Note that this is erroneous, you can't drop a IS-AM on the table
+	2. The only way is to drop primary key constraint
+	https://blog.sqlauthority.com/2016/08/14/drop-clustered-index-primary-key-column-interview-question-week-084/
+	3. There is a need to constantly insert, update and delete indexes
+	according to changing needs.
+	4. However, the performance itself can be costly.
+	5. For clustered index, the table will be physically reordered, just
+	imagine you have 10M records. Also, if there are nonclustered indexes,
+	all of the pointers to seek clustered index will be turned into RID.
+	https://stackoverflow.com/a/55027586
+	https://stackoverflow.com/a/47603655
+*/
+
+drop table tblTest;
