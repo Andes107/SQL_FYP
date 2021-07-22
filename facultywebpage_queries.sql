799de4a51907412797b6ff7fa68a689dda2709d8
Andes Lam <andeslam223@gmail.com>
2021-07-22 11:16:57 +0800

    added remark on changing CIs on PK changes

--- facultywebpage_queries.sql
+++ facultywebpage_queries.sql
@@ -21,6 +21,18 @@ alter table tblSupervises
 drop constraint PK_tblSupervises;
 alter table tblSupervises
 add constraint PK_tblSupervises primary key(username, fypId);
+/*
+	1. The original table tblSupervises has set primary key as fypId and username
+	2. Since the join attribute is on username and order matters
+	3. A new primary key and subsequent clustered index are created
+	4. It is not feasible if there are a lot of records because
+		a. Dropping primary key will drop the clustered index if it exists
+		b. The table will be reorganized into a heap
+		c. all NCIs leaf node will rebuild into RID
+		d. after adding new primary key, all NCIs leaf node will rebuild back to clustered key
+	5. Is there a solution? no, so if shit hits the fan, please refer to the following
+		https://dba.stackexchange.com/a/172104
+*/
 
 create nonclustered index IX_tblFyp_title
 on tblFyp(title)
