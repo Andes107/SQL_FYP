a1416df7bda003ae1592654794e498c2e2d5d797
Andes Lam <andeslam223@gmail.com>
2021-07-22 10:47:26 +0800

    modified sp1

--- facultywebpage_queries.sql
+++ facultywebpage_queries.sql
@@ -1,10 +1,12 @@
+drop proc if exists sp1;
+
 /*
 TODO 01: Used in Faculty\DisplayFYPs
 Construct the SQL SELECT statement to retrieve the fyp id, title, category, type, 
 minimum students and maximum students of the FYPs supervised by a faculty identified
 by his/her username. Order the result by title ascending.
 */
-Create Proc sp1
+create proc sp1
 @username varchar(15)
 as
 begin
@@ -23,3 +25,5 @@ add constraint PK_tblSupervises primary key(username, fypId);
 create nonclustered index IX_tblFyp_title
 on tblFyp(title)
 include(PK_tblFyp,category,type,minStudents,maxStudents);
+
+exec sp1 'fan';
